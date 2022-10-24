9fcd4d76de8e3c565a7e4fe22278687237051aa8
Aung Paing Soe <aps@pop-os.localdomain>
2022-10-24 06:30:44 +0630

    dynamic component props & emit

--- src/views/ModelView.vue
+++ src/views/ModelView.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <h1>Model</h1>
+    <p>Show Dark Model</p>
+    <input type="checkbox" v-model="darkModel" >
+    <br>
     <button @click="showModel = true">show model</button>
-    <!-- Should be html -->
-    <Teleport to="body"> 
+    <component :is=" darkModel ? DarkModel : Model " v-if="showModel" @hideModel="showModel = false" title="Title From Props">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+        incidunt assumenda in alias quas unde magni facilis, blanditiis nesciunt
+        inventore dignissimos sint quod aliquam maiores cumque doloremque quam
+        eum commodi.
+      </p>
+    </component>
+    <!-- Teleport Should be Html Attribute -->
+    <!-- <Teleport to="body"> 
       <div class="model" v-if="showModel"> 
         <h1>Model Title</h1>
         <p>
@@ -14,25 +25,15 @@
           <button @click="showModel = false">hide model</button>
         </p>
       </div>
-    </Teleport>
+    </Teleport> -->
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Model from "../components/Model.vue";
+import DarkModel from "../components/DarkModel.vue"
 
 const showModel = ref(false);
+const darkModel = ref(false);
 </script>
-
-<style scoped>
-.model {
-  padding: 10px;
-  background-color: blueviolet;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-}
-</style>
